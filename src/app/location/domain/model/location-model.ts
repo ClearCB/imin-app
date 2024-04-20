78484e5088c367941fa7a9793a8e6b7b8d5ca689
ClearCB <abelcasasccb@gmail.com>
2024-04-20 13:43:15 +0200

    fix: location model

--- src/app/location/domain/model/location-model.ts
+++ src/app/location/domain/model/location-model.ts
@@ -3,24 +3,24 @@ import { LOCATION_CONSTANTS } from "../../location-constants";
 export interface LocationModel {
 
     id: string;
-    title: string;
-    smallDescription: string;
-    largeDescription: string;
+    name: string;
+    latitude: string;
+    longitude: string;
 
 }
 
 export function ensureLocationIsValid(location: LocationModel): void {
 
-    if (!isValidModelTitle(location.title)) {
-        throw LocationTitleNotValidError(location.title);
+    if (!isValidModelName(location.name)) {
+        throw LocationNameNotValidError(location.name);
     }
 
 }
 
-export function isValidModelTitle(title: string): boolean {
+export function isValidModelName(title: string): boolean {
     return title != null && title != undefined && title != '';
 }
 
-export function LocationTitleNotValidError(title: string): Error {
+export function LocationNameNotValidError(title: string): Error {
     return new Error(`${LOCATION_CONSTANTS.MESSAGES.LOCATION_CANT_BE_NULL}: ${title}`)
 }
