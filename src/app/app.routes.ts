35eae0a07a526669661ac9e32ce9313a354c3177
ClearCB <abelcasasccb@gmail.com>
2024-04-26 18:49:39 +0200

    feat: add icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,20 +5,31 @@ import { GeneralErrorComponent } from './shared/infrastructure/view/error/genera
 import { authGuard } from './auth/infrastructure/guard/auth.guard';
 import { LayoutComponent } from './shared/infrastructure/view/layout/layout.component';
 import { EventListComponent } from './event/infrastructure/view/event-list/event-list.component';
-import { RegisterComponent } from './auth/infrastructure/view/register/register.component';
 import { NotFoundErrorComponent } from './shared/infrastructure/view/error/not-found-error/not-found-error.component';
 import { ForbiddenErrorComponent } from './shared/infrastructure/view/error/forbidden-error/forbidden-error.component';
+import { EventCreateFormComponent } from './event/infrastructure/view/event-create-form/event-create-form.component';
+import { EventDetailComponent } from './event/infrastructure/view/event-detail/event-detail/event-detail.component';
+import { MapLayoutComponent } from './map/infrastructure/view/map-layout/map-layout.component';
+import { EventMapComponent } from './event/infrastructure/view/event-map/event-map.component';
+import { ProfileComponent } from './account/infrastructure/view/profile/profile.component';
+import { CustomCalendarComponent } from './event/infrastructure/view/custom-calendar/custom-calendar.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' }, // Empty route => home
     { path: 'login', component: LoginComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+    { path: 'calendar', component: CustomCalendarComponent, canActivate: [authGuard] },
     {
         path: 'home', component: LayoutComponent,
         children: [
             { path: '', component: DashboardComponent },
-            { path: 'event', data: { breadcrumb: 'Prime Icons' }, component: EventListComponent },
-            { path: 'register', component: RegisterComponent },
-            { path: 'new-event', component: EventListComponent, canActivate: [authGuard] },
+            {
+                path: 'event-map', component: EventMapComponent,
+                children: [{ path: "", component: MapLayoutComponent }]
+            },
+            { path: 'event-list', component: EventListComponent },
+            { path: 'event-new', component: EventCreateFormComponent, canActivate: [authGuard] },
+            { path: 'event-detail', component: EventDetailComponent },
         ]
     },
     { path: 'error', component: GeneralErrorComponent, },
